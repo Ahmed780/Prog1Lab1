e0e73447a1f00bb02eccd2b3b688da9b815573e0
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-02-02 03:43:49 -0500

    editted the file

---
+++ MyAgeIs.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+public class Age {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        // second line 
+       
+
+
+        
+  int currentYear = 2020;
+  int yearofBirth = 1998;
+
+  	int age = currentYear - yearofBirth;
+
+  	System.out.println("My age is " + age);
+
+    }
+
+}
