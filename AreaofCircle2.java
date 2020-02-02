76d6d851ddb354ee28d21f8623d3e934b36f52bf
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-02-02 03:44:56 -0500

    editted the file

---
+++ AreaofCircle2.java
@@ -0,0 +1,26 @@
+
+
+
+
+
+public class AreaofCircle2 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        // second line 
+       
+System.out.println("hru ");
+
+			final double width = 5;
+			double height = 10;
+			double area;
+			double perimeter = 2*(height + width);
+			
+			area = width* height;
+
+System.out.println("The area of the triangle is " + area);
+ System.out.printf("The perimeter is " + perimeter);       
+  
+    }
+
+}
