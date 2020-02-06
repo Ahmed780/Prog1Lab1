e1e8058a76f1346c9be5cbef3e273c4a5f201b7b
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-02-05 21:12:56 -0800

    file

--- currencyconverter.java
+++ currencyconverter.java
@@ -1,36 +1,31 @@
 
 
-/******************************************************************************
- *  Compilation:  javac HelloWorld.java
- *  Execution:    java HelloWorld
- *
- *  Prints "Hello, World". By tradition, this is everyone's first program.
- *
- *  % java HelloWorld
- *  Hello, World
- *
- *  These 17 lines of text are comments. They are not part of the program;
- *  they serve to remind us about its properties. The first two lines tell
- *  us what to type to compile and test the program. The next line describes
- *  the purpose of the program. The next few lines give a sample execution
- *  of the program and the resulting output. We will always include such 
- *  lines in our programs and encourage you to do the same.
- *
- ******************************************************************************/
-
-public class Converter {
+public class currencyconverter {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("converter");
-        double distanceInMeters;
-        double distanceInFeet;
-        final double factor = 3.28084;
+        
+        double currency;
+        double currencyUSD;
+        final double factor = 0.75;
+		double currencyEUR;
+        final double factor2 = 0.60;
+		
+		
 
-        distanceInMeters = 5;
-        distanceInfeet = factor * distanceInMeters;
+        currency = 2;
+        
+		
+  
+		currencyUSD = factor * currency;
+		currencyEUR = factor2 * currency;
+		
+		
 
-	    System.out.println("\n" + distanceInMeters + "meters is equal to + distanceInFeet + "feet.");
+	    System.out.println( + currency + " CAD is equal to " + currencyUSD + " USD.");
+		System.out.println( + currency + " CAD is equal to " + currencyEUR + " EUR.");
+		
 
 
     }
