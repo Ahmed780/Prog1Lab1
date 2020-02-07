c4851e49cbd53a93a67d8838244055e4234605e2
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-02-06 17:35:17 -0800

    Add files via upload

---
+++ textbook/X.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+public class X {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        // second line 
+       
+
+     
+  	System.out.println("*	* ");
+  		System.out.println("   * * ");
+  	System.out.println( "    * ");
+  	System.out.println("   * * ");
+  		System.out.println("*	* ");
+
+    }
+
+}
---
+++ textbook/star.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+public class star {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        // second line 
+       
+
+     
+  	System.out.println("********** ");
+
+    }
+
+}
